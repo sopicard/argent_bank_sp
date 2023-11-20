7d23510997f00a534a3361456a987d52a0723d80
sopicard <sogautrelet@gmail.com>
2023-11-20 23:13:08 +0100

    Initial commit : configuration of MongoDB Atlas connection and update project structure

--- back_end/server/database/connection.js
+++ back_end/server/database/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL
+  // console.log(`Connecting to database with URL: ${databaseUrl}`) 
+
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
--- back_end/server/server.js
+++ back_end/server/server.js
@@ -1,12 +1,10 @@
+require('dotenv').config()
 const express = require('express')
-const dotEnv = require('dotenv')
 const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('./swagger.yaml')
 const dbConnection = require('./database/connection')
-
-dotEnv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3001
