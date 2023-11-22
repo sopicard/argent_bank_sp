8da826ab8565f2660ec76dc1a1f5f83e76e499ae
sopicard <sogautrelet@gmail.com>
2023-11-22 22:48:56 +0100

    react-router-dom + Navbar and Home components

---
+++ front_end/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './Navbar';
+// import Hero from './Hero';
+// import Features from './Features';
+// import Footer from './Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* <main>
+        <Hero />
+        <Features />
+      </main>
+      <Footer /> */}
+    </div>
+  );
+};
+
+export default Home;
